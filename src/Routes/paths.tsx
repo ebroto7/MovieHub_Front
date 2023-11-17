ebfcb371b5f3686ed11f0cf1824f30071fd6eaec
Enric Broto Hernandez <enrique7siete@gmail.com>
2023-11-17 10:28:11 +0100

    (add) auth0 init - loginLogoutButton

--- src/Routes/paths.tsx
+++
@@ -1,9 +0,0 @@
-// export const LANDING = '/landing'
-export const HOME = '/Home'
-
-// export const LOGIN = '/login'
-// export const SIGNUP = '/signup'
-
-// export const SEARCH = '/search'
-
-export const PRIVATE = '/*'
